114f9c277afaaa3b7c02e5f88520096641f63f78
summitgautam <summitbro1@gmail.com>
2026-07-05 09:30:27 +0545

    FINALLY COMPLETED THE REDIRECTION PROCESS LESS GOOOOOOOOOOOOOOOOOOO now there are some database stuffs to do and then the whole project will be completed :YAY:

--- public/script.js
+++ public/script.js
@@ -1,21 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const urlForm = document.querySelector('form'); 
-
+   
     if (urlForm) {
         urlForm.addEventListener('submit', (e) => {
             e.preventDefault(); 
-
+             
             const urlInput = document.querySelector('input[name="url"]').value;
-
+            const random = Math.random().toString(36).slice(2,7);
+            console.log(random);
             fetch('/save', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ url: urlInput }) 
+                body: JSON.stringify({ url: urlInput, randomcode:random })
             })
             .then(response => response.json()) 
             .then(result => {
                 if (result.success) {
                     localStorage.setItem('processedUrl', result.url);
+                    localStorage.setItem('randomcode', result.randomcode);
                     window.location.href = '/';
                 } else {
                     console.error('No URL returned from backend');
--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+// const random_code = crypto.randomBytes(5).toString('base64url')
+let url = null;
+let random_code = null;
 
 app.post('/save', (req,res)=>{
-    const url = req.body.url;
+    url = req.body.url;
+    random_code = req.body.randomcode;
+    console.log(random_code);
     console.log(`The Url is ${url}`);
+
     res.json({
         success: true,
         url: url
     })
 })
+app.get('/:random_code', (req,res)=>{
+    if(!url || !random_code || req.params.random_code !== random_code){
+        return res.status(400).send("Error: No url or random code found.");
+    }
+    res.redirect(url);
+});
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
 })
